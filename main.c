226eedf2bc09f7ea674c360ba965ce2d7ebfbe48
pav67 <avart.paul@gmail.com>
2014-08-07 12:38:53 +0200

    cleaning main.c

--- main.c
+++ main.c
@@ -6,38 +6,6 @@
 
 int main(int argc, char** argv){
 
-	/*list *ll = init_list();
-	node *n1 = add_list(ll, (void*) "hello1");
-	node *n2 = add_list(ll, (void*) "hello2");
-	node *n3 = add_list(ll, (void*) "hello3");
-	node *n4 = add_list(ll, (void*) "hello4");
-	browse_list(ll, (*print_node));
-	delete_node(ll, n4);
-	browse_list(ll, (*print_node));
-	delete_list(ll);
-	browse_list(ll, (*print_node));*/
-
-	/*node* n;
-
-	crecre *cre1 = create_crecre("FTTRR");
-	crecre *cre2 = create_crecre("MTTBB");
-	crecre *cre3 = create_crecre("FTTBR");
-	crecre *cre4 = create_crecre("MTTRB");
-	crecre *cre5 = create_crecre("MTTRR");
-
-	list *ll = init_list();
-	add_list(ll, cre1, delete_crecre, print_crecre);
-	add_list(ll, cre2, delete_crecre, print_crecre);
-	add_list(ll, cre3, delete_crecre, print_crecre);
-	add_list(ll, cre4, delete_crecre, print_crecre);
-	add_list(ll, cre5, delete_crecre, print_crecre);
-
-	//browse_list(ll, print_crecre);
-	for(n=ll->head;n;n=n->next)
-		n->print_callback(n->data);
-	
-	delete_list(ll);*/
-	
 	srand(time(NULL));
 
 	genome* fg1 = create_genome('T', 'T', 'T');
